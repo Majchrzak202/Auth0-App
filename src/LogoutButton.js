b6b43484602327df5ebda1b56debf364adf4b54e
MarcinMaster <m.luba505@gmail.com>
2022-05-08 06:01:11 +0200

    add an not see logout when not log in logic

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -4,8 +4,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
 
-    const {logout} = useAuth0(); 
-    return (
+    const {logout, isAuthenticated} = useAuth0(); 
+    return  isAuthenticated && (
         <div>
             <button onClick={logout}>Logout!</button>
         </div>
