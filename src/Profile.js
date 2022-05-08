0a12c51dc3ff7a748ee74376c053ff8e5088a4c6
MarcinMaster <m.luba505@gmail.com>
2022-05-08 06:01:44 +0200

    Added an Profile changes to perform task for user

--- src/Profile.js
+++ src/Profile.js
@@ -10,12 +10,13 @@ const Profile = () => {
     }
  
     return ( 
-        isAuthenticated && (
+         isAuthenticated &&  (
      <div>
          <img src={user.picture} alt={user.name}/>
          <h2>{user.name}</h2>
          <p>{user.email}</p>
-         {JSON.stringify(user)}
+         {/* // to see the user object fields
+          {JSON.stringify(user)} */}
     </div>
     )
     )
